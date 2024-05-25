0ed6353adef5a2929e35fde2c7e1eb073c34a80d
uzielabisai <uzielabisaim@gmail.com>
2024-05-25 01:54:25 -0600

    Create suma.cc

---
+++ Ejercicios programación/java/suma.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int contador = 1;
+    int suma = 0;
+
+    cout << "Introduce un numero" << endl;
+    cin >> suma;
+
+    if(contador <= 100, contador + suma, contador++){
+
+    }
+
+    return 0;
+}
